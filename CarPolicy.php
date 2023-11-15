41bebf3535528c5c351b80e7e4789b842c121d2d
B00144945 <B00144945@mytudublin.ie>
2023-11-15 10:21:31 +0000

    CarPolicy Part 1

---
+++ CarPolicy.php
@@ -0,0 +1,34 @@
+<?php
+class CarPolicy {
+    private $policyNumber;
+    private $yearlyPremium;
+    private $dateOfLastClaim;
+
+    public function __construct($policyNumber, $yearlyPremium) {
+        $this->policyNumber = $policyNumber;
+        $this->yearlyPremium = $yearlyPremium;
+        $this->dateOfLastClaim = null;
+    }
+
+    public function setDateOfLastClaim($date) {
+        $this->dateOfLastClaim = $date;
+    }
+
+    public function getTotalYearsNoClaims() {
+        $currentDate = new DateTime();
+        $lastClaimDate = new DateTime($this->dateOfLastClaim);
+        $interval = $currentDate->diff($lastClaimDate);
+        return $interval->format("%y");
+    }
+
+    public function __toString() {
+        return $this->policyNumber . ': ' . 'Unknown';
+    }
+
+    public function getNoClaimsString() {
+        return $this->__toString() . ' has ' . $this->getTotalYearsNoClaims() . ' years no claims.';
+    }
+}
+?>
+
+
